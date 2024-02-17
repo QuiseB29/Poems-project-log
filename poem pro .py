7173a0135c57763d49bc35fa1fa53ab33ae5e4ea
QuiseB29 <bmarquise544@gmail.com>
2024-02-17 17:57:10 -0600

    Create a Poem using Comments

---
+++ poem pro .py
@@ -0,0 +1,10 @@
+# Python, oh Python, so clear and so neat
+print ("Python, oh Python, so clear and so neat")
+# With every new challenge, you're hard to beat.
+print ("With every new challenge, you're hard to beat")
+# Through every challenge, I shall not quit.
+print ("Through every challenge, I shall not quit")
+# Any problem or crack, It will get fixed.
+print ("Any Problem or crack, It will get fixed")
+# Dont give up, Dont quit.
+print ("Dont give up, Dont quit.")
